3387891bc271e8cdcf4196018dd27f82a450abf1
Edward Ramos <edward.ramos@procer.com.br>
2024-11-21 12:28:26 -0300

    feat(ptf-006): footer social

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { ButtonModule } from 'primeng/button';
@@ -19,15 +19,23 @@ import { SpeedDialModule } from 'primeng/speeddial';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
   public checked: boolean = false;
   public nameButton: string = 'Dark Mode';
-  public colorButton: string = 'contrast';
+  public colorButton: string = 'dark-mode-btn';
+
+  ngOnInit(): void {
+    
+  }
 
   public toggleDarkMode(): void {
     this.checked = !this.checked;
     document.body.classList.toggle('light-theme');
     this.nameButton = this.checked ? 'Light Mode' : 'Dark Mode';
     this.colorButton = this.checked ? 'light-mode-btn' : 'dark-mode-btn';
+  }
+
+  public setLanguage(lang: string = 'pt'): void {
+    console.log(lang)
   }
 }
